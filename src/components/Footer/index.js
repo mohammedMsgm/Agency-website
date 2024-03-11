ff86aed4d8bf2c29ab8e1b7f77109544c7fa970f
mohammedMsgm <mohmsgm2003@gmail.com>
2024-03-11 12:50:53 +0100

    changing logo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -48,21 +48,21 @@ const Footer = () => {
     <FOOTER>
       <LeftText>
         © 2021 Built and Design by{" "}
-        <a href="https://www.youtube.com/channel/UCeYt6blRBKuNrEg_-282fSA">
+        <a href="https://www.youtube.com/channel/externcom-dz">
           @ExternCom
         </a>
       </LeftText>
       <RightText>
         Reach out to me via 😉
-        <a href="https://twitter.com/code_bucks">
+        <a href="https://twitter.com/externcom.dz">
           <img src={Twitter} alt="Twitter" />
         </a>
         &nbsp;
-        <a href="https://www.instagram.com/code.bucks/">
+        <a href="https://www.instagram.com/externcom.dz/">
           <img src={Instagram} alt="Instagram" />
         </a>
         &nbsp;
-        <a href="mailto:ExternCom27@gmail.com?subject=Email From Your Website">
+        <a href="mailto:Contact@externcom.dz?subject=Email From Your Website">
           <img src={Gmail} alt="Gmail" />
         </a>
       </RightText>
